02590fce5af7fbfa34a190d71cdfa83298fcf953
dowenliu-xyz <hawkdowen@hotmail.com>
2020-03-27 21:34:08 +0800

    loop

---
+++ src/bin/08-02-00-loop.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    // 无限循环
+    let o = loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            // 跳过这次迭代的剩下内容
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            // 退出循环
+            break 1;
+        }
+    };
+
+    println!("{}", o)
+}
