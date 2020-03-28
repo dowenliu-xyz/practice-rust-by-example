6ce01eaed78f5cf73090db4cc28710d6ee113bc9
dowenliu-xyz <hawkdowen@hotmail.com>
2020-03-28 17:01:33 +0800

    capture

--- src/bin/09-02-01-capture.rs
+++ src/bin/09-02-01-capture.rs
@@ -49,4 +49,19 @@ fn main() {
     consume();
     // consume();
     // ^ 试一试：将此行注释去掉。
+
+    // `Vec` 在语义上是不可复制的。
+    let haystack = vec![1, 2, 3];
+
+    let contains = move |needle| haystack.contains(needle);
+
+    println!("{}", contains(&1));
+    println!("{}", contains(&4));
+
+    // println!("There're {} elements in vec", haystack.len());
+    // ^ 取消上面一行的注释将导致编译时错误，因为借用检查不允许在变量被移动走
+    // 之后继续使用它。
+
+    // 在闭包的签名中删除 `move` 会导致闭包以不可变方式借用 `haystack`，因此之后
+    // `haystack` 仍然可用，取消上面的注释也不会导致错误。
 }
